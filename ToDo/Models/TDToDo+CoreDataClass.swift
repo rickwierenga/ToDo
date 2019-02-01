2b4f814009675974dec3e8d27b4c036ab625c64f
rickwierenga <rick_wierenga@icloud.com>
2019-02-01 15:38:56 +0100

    project setup

---
+++ ToDo/Models/TDToDo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  TDToDo+CoreDataClass.swift
+//  ToDo
+//
+//  Created by Rick Wierenga on 01/02/2019.
+//  Copyright © 2019 Rick Wierenga. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class TDToDo: NSManagedObject {
+
+}
---
+++ ToDo/Models/TDToDo+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  TDToDo+CoreDataProperties.swift
+//  ToDo
+//
+//  Created by Rick Wierenga on 01/02/2019.
+//  Copyright © 2019 Rick Wierenga. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension TDToDo {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TDToDo> {
+        return NSFetchRequest<TDToDo>(entityName: "TDToDo")
+    }
+
+    @NSManaged public var done: Bool
+    @NSManaged public var name: String?
+
+}
